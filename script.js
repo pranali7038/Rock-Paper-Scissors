a6ac737523dfc25fc460237d62e67c9956489400
pranali7038 <22510663.dypit@dypvp.edu.in>
2024-06-21 11:58:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg  = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score"); 
+const compScorePara = document.querySelector("#comp-score");
+
+const genCompChoice = () =>{
+    const options =["rock", "paper","scissors"];
+    const randIdx = Math.floor(Math.random() * 2);
+
+    return options[randIdx];
+}
+
+const drawGame = ()=>{
+    msg.innerText = "Game was draw,Play again!!";
+    msg.style.backgroundColor = "#081b31";
+}
+
+const showWinner = (userWin,userChoice,compChoice)=>{
+    if(userWin){
+        userScore++;
+        userScorePara.innerText = userScore;
+
+        msg.innerText = `You Win! Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "Green";
+    }else{
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText =`You Loose!  ${compChoice} beats Your ${userChoice}`;
+        msg.style.backgroundColor = "red";
+    }
+}
+
+const playGame = (userChoice)=>{
+    console.log("User Choice",userChoice);
+
+    //generate comp choice
+    const compChoice = genCompChoice();
+    console.log("Comp Choice",compChoice);
+
+    if(userChoice === compChoice){
+        drawGame();
+    }else{
+        let userWin = true;
+        if(userChoice === "rock"){
+            //scissors,paper
+            userWin=compChoice === "paper" ?false : true;
+        }else if (userChoice == "paper"){
+            //rock,scissors
+            userWin =compChoice ==="scissors"?false:true;
+        }else{
+            //rock,paper
+            userWin = compChoice ==="rock"?false:true;
+        }
+
+        showWinner(userWin,userChoice,compChoice);
+    }
+};
+
+
+choices.forEach((choice) =>{
+    choice.addEventListener("click", ()=>{
+        const userChoice =choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
